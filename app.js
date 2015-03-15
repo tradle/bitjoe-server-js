6b81defbba08dcdaf6dd62d5a529b191a01aa902
Mark Vayngrib <mark.vayngrib@lablz.com>
2015-03-14 20:23:44 -0400

    set bitjoe config directly, use higher port

--- app.js
+++ app.js
@@ -3,24 +3,12 @@
 var Joe = require('bitjoe-js');
 var server = require('./');
 var minimist = require('minimist');
-var fs = require('fs');
-var path = require('path');
 var argv = minimist(process.argv);
+var conf = require('./conf/config');
 
-var configPath;
-if (argv.joe)
-  configPath = path.join(__dirname, argv.joe);
-else
-  configPath = path.join(__dirname, 'node_modules/bitjoe-js/conf/config.json');
+var port = argv.port || conf.port;
+var joe = new Joe(conf.bitjoe);
 
-var config = fs.readFileSync(configPath, {
-  encoding: 'utf8'
-});
-config = JSON.parse(config);
-
-var port = argv.port || require('./conf/config.json').port;
-
-var joe = new Joe(config);
 joe.on('ready', function() {
   console.log('Bitjoe is ready, starting server...');
   server.create(joe, port);
