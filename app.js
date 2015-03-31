e717e44b86490f2d242eed4cfdf369f28b6e5099
Mark Vayngrib <mark.vayngrib@lablz.com>
2015-03-31 14:01:50 -0400

    renames for readability

--- app.js
+++ app.js
@@ -8,26 +8,24 @@ var url = require('url');
 var conf = require('./conf/config');
 var joe = new Joe(conf.bitjoe);
 
-var serverConf = {
-  bitjoe: joe,
-  port: argv.port || conf.port,
-  ipWhitelist: [
-    '127.0.0.1',
-    'localhost'
-  ]
-};
+conf.bitjoe = joe;
+conf.port = argv.port || conf.port;
+conf.ipWhitelist = [
+  '127.0.0.1',
+  'localhost'
+];
 
 if (argv.docker) {
   var host = process.env.DOCKER_HOST;
   console.log('Adding docker host to ip whitelist: ' + host);
   if (host) {
     host = url.parse(host);
-    serverConf.ipWhitelist.push(host.hostname);
-    console.log('IP whitelist: ' + JSON.stringify(serverConf.ipWhitelist, null, 2));
+    conf.ipWhitelist.push(host.hostname);
+    console.log('IP whitelist: ' + JSON.stringify(conf.ipWhitelist, null, 2));
   }
 }
 
 joe.on('ready', function() {
   console.log('Bitjoe is ready, starting server...');
-  server.create(serverConf);
+  server.create(conf);
 });
